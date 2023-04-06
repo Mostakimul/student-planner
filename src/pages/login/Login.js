455b5c6e4b632f3bbd7d00a2c9164eb617dd19c6
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-04-05 23:13:59 -0400

    feat: login page designed

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import Logo from '../../assets/images/logo-big.png';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex items-center">
+      <div className="flex-1 flex items-center justify-center">
+        <img src={Logo} alt="logo" className="h-96" />
+      </div>
+      <div className="flex-1">
+        <div className="mb-10">
+          <h1 className="text-5xl mb-2 text-gray-200">Login</h1>
+          <div className="h-1 w-2/4 bg-yellow-500"></div>
+        </div>
+        <form action="#" className="space-y-5">
+          <div>
+            <input
+              className="w-3/4 p-2 rounded-md"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div>
+            <input
+              className="w-3/4 p-2 rounded-md"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <button className="bg-yellow-500 text-gray-900 px-5 py-2 rounded-md">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
