58b7b550d5ad9bf41c78cb1a42f0c106a17e3028
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-04-05 22:27:39 -0400

    feat: tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="flex items-center h-screen text-gray-300 font-ubuntu">
+      <div className="glass-bg w-3/4 mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        excepturi velit deleniti error eligendi aperiam aliquid commodi
+        repellendus dolores natus minus corporis consectetur nesciunt veniam,
+        tempore neque quisquam officiis, recusandae reiciendis quasi
+        consequuntur a. Debitis optio sed nam, labore nobis quae error ut
+        recusandae facilis aperiam doloribus impedit vitae vel consequatur
+        dolorum architecto? Illum suscipit accusamus cupiditate, minima et sequi
+        asperiores nostrum maiores laborum quas dolores rerum amet est
+        assumenda, dicta qui deserunt delectus facere dolore, sit quia.
+        Assumenda accusantium illo recusandae provident. Vitae minima obcaecati
+        velit deserunt corrupti temporibus ipsam harum ratione maxime deleniti
+        excepturi explicabo, quasi, quos sit.
+      </div>
+    </div>
+  );
 }
 
 export default App;
