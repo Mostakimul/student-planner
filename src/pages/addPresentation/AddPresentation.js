2e3920661ae4c1bcadc373eb2b0f275fbc89d4fc
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-05-01 00:33:39 -0400

    feat: added sample pages

---
+++ src/pages/addPresentation/AddPresentation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AddPresentation = () => {
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <h1 className="text-center bg-yellow-600 max-w-max px-8 py-4 rounded-md font-bold text-xl">
+        Add Presentations
+      </h1>
+      <div className="mt-5">Presentations input goes here</div>
+    </div>
+  );
+};
+
+export default AddPresentation;
---
+++ src/pages/presentations/Presentations.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Presentations = () => {
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <h1 className="text-center bg-yellow-600 max-w-max px-8 py-4 rounded-md font-bold text-xl">
+        Presentations
+      </h1>
+      <div className="mt-5">Presentations table goes here</div>
+    </div>
+  );
+};
+
+export default Presentations;
