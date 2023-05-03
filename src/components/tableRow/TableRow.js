182c5202e6101a5284465abfe3604cdc14719951
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-05-03 01:22:40 -0400

    feat: dummy classes added

---
+++ src/components/tableRow/TableRow.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "../button/Button";
+
+const TableRow = ({ data }) => {
+  const { id, subject, professor, schedule, room } = data;
+
+  const editHandler = (id) => {
+    console.log("Edit class ", id);
+  };
+  const deleteClass = (id) => {
+    console.log("Delete class", id);
+  };
+
+  return (
+    <tr>
+      <td className="border border-slate-600 p-1">{subject}</td>
+      <td className="border border-slate-600 p-1">{professor}</td>
+      <td className="border border-slate-600 p-1">{schedule}</td>
+      <td className="border border-slate-600 p-1">{room}</td>
+      <td className="border border-slate-600 p-1 space-x-3">
+        <Button
+          clikHandler={() => editHandler(id)}
+          classNames={"bg-blue-600 py-1 px-4"}
+        >
+          Edit
+        </Button>
+        <Button
+          clikHandler={() => deleteClass(id)}
+          classNames={"bg-red-600 py-1 px-4"}
+        >
+          Delete
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+export default TableRow;
--- src/pages/classes/Classes.js
+++ src/pages/classes/Classes.js
@@ -1,5 +1,30 @@
 import React from "react";
 import LogoutButton from "../../components/logoutButton/LogoutButton";
+import TableRow from "../../components/tableRow/TableRow";
+
+const dummyClass = [
+  {
+    id: "1",
+    subject: "ENMG 652",
+    professor: "Chris Hauge",
+    schedule: "7:00 pm to 9:30 pm",
+    room: "412",
+  },
+  {
+    id: "2",
+    subject: "SENG 638",
+    professor: "Dr. Mohammad Samarah",
+    schedule: "4:30 pm to 7:00 pm",
+    room: "422",
+  },
+  {
+    id: "3",
+    subject: "SENG 645",
+    professor: "Dr. Mohammad Samarah",
+    schedule: "4:30 pm to 7:00 pm",
+    room: "421",
+  },
+];
 
 const Classes = () => {
   return (
@@ -8,7 +33,24 @@ const Classes = () => {
       <h1 className="text-center bg-yellow-600 max-w-max px-8 py-4 rounded-md font-bold text-xl">
         Classes
       </h1>
-      <div className="mt-5">inputs goes here</div>
+      <div className="mt-5">
+        <table className="border-separate border-spacing-2 border border-slate-500">
+          <thead>
+            <tr>
+              <th className="border border-slate-600 p-1">Subject</th>
+              <th className="border border-slate-600 p-1">Professor</th>
+              <th className="border border-slate-600 p-1">Schedule</th>
+              <th className="border border-slate-600 p-1">Room</th>
+              <th className="border border-slate-600 p-1">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dummyClass.map((myClass) => (
+              <TableRow key={myClass.id} data={myClass} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
